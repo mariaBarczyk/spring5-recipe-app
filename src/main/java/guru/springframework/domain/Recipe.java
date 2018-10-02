bce7f5c44de52b4e3702a38cb899f997313b8971
mariaBarczyk <barczyk.maria92@gmail.com>
2018-10-02 14:46:53 +0200

    Initialize collections, add setting bideractional relation with ingredient

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -2,6 +2,7 @@ package guru.springframework.domain;
 
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -17,10 +18,12 @@ public class Recipe {
     private Integer servings;
     private String source;
     private String url;
+
+    @Lob
     private String directions;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
-    private Set<Ingredient> ingredients;
+    private Set<Ingredient> ingredients = new HashSet<>();
 
     @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
@@ -35,7 +38,10 @@ public class Recipe {
     @JoinTable(name="recipe_category",
         joinColumns = @JoinColumn(name="recipe_id"),
         inverseJoinColumns = @JoinColumn(name="category_id"))
-    private Set<Category> categories;
+    private Set<Category> categories = new HashSet<>();
+
+    public Recipe() {
+    }
 
     public Long getId() {
         return id;
@@ -115,6 +121,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+        notes.setRecipe(this);
+    }
+
+    public Recipe addIngredient(Ingredient ingredient) {
+        ingredient.setRecipe(this);
+        ingredients.add(ingredient);
+        return this;
     }
 
     public Set<Ingredient> getIngredients() {
